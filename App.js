5ab3862cc0be2acaba9a2de55d300c0be532ca1c
BrijeshParihar <80513034+BrijeshParihar@users.noreply.github.com>
2023-09-30 17:44:49 +0530

    day03

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
 import React from "react"
-import ReactDOM from "react-dom";
-const heading = React.createElement("h1",{},"This react app");
+import ReactDOM from "react-dom/client";
+//   
+const jsxheading = (
+    <h1 className="head" tableIndex="5">
+    This is from using jsx 
+    </h1> );
+console.log(jsxheading);
+
+
+const HeadingComponent1 = ()=> <div id ="container"><h1 className="heading"> This is functional component</h1></div>
+const HeadingComponent = ()=> 
+(<div id ="container">
+    <HeadingComponent1/>
+    <h1 className="heading"> This is functional component</h1>
+    </div>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
